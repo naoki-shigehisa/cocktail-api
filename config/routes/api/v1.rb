3127fc807a25f5008aa4c45252c5dea9b936f9a2
naoki-shigehisa <naoki.shigehisa@gmail.com>
2023-02-13 21:07:08 +0900

    add techniques

--- config/routes/api/v1.rb
+++ config/routes/api/v1.rb
@@ -2,4 +2,5 @@ namespace :v1 do
   resources :materials, only: [:index, :show]
   resources :material_groups, only: [:index]
   resources :tools, only: [:index]
+  resources :techniques, only: [:index]
 end
