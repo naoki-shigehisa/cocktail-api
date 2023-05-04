5b27ca617462ddde80882412e300776a58643837
naoki-shigehisa <naoki.shigehisa@gmail.com>
2023-05-04 23:12:51 +0900

    modify view of simple recipe

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RecipesController < ApplicationController
   LATEST_RECIPE_LIMIT = 10
 
   def index
-    @recipes = Recipe.all.preload(:color, :technique, :type, :taste)
+    @recipes = Recipe.all.preload(recipe_material_maps: [:material])
   end
 
   def show
@@ -11,7 +11,7 @@ class Api::V1::RecipesController < ApplicationController
 
   def latests
     @recipes = Recipe
-      .preload(:color, :technique, :type, :taste)
+      .preload(recipe_material_maps: [:material])
       .order(created_at: :desc)
       .limit(LATEST_RECIPE_LIMIT)
   end
