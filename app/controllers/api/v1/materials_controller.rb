804d82e53090338514a39503f829b69de83eabc7
naoki-shigehisa <naoki.shigehisa@gmail.com>
2023-05-07 01:57:49 +0900

    fix show material

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -5,5 +5,6 @@ class Api::V1::MaterialsController < ApplicationController
 
   def show
     @material = Material.find(params[:id])
+    @material_recipes = Recipe.active.where(id: RecipeMaterialMap.where(material_id: @material.id).pluck(:recipe_id))
   end
 end
