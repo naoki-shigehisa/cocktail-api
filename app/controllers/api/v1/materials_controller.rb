11011f36b6bfe90085a3ddb7430bab9dfcf056ea
naoki-shigehisa <naoki.shigehisa@gmail.com>
2023-05-14 22:07:07 +0900

    modify order of material_recipe

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::MaterialsController < ApplicationController
 
   def show
     @material = Material.find(params[:id])
-    @material_recipes = Recipe.active.where(id: RecipeMaterialMap.where(material_id: @material.id).pluck(:recipe_id))
+    @material_recipes = Recipe.active.where(id: RecipeMaterialMap.where(material_id: @material.id).pluck(:recipe_id)).reverse
   end
 
   def random
